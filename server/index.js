0ea4dd193321b56c6c3bdfa554653e8dc0ff904e
Kristian B. Wagner <kristianbwagner@gmail.com>
2018-07-22 09:15:19 +0200

    updated the port on heroku dev

--- server/index.js
+++ server/index.js
@@ -5,4 +5,4 @@ const app = express();
 require("./routes")(app);
 
 const port = process.env.port || 3000;
-app.listen(3000, () => console.log(`server api running on port ${port}`));
+app.listen(port, () => console.log(`server api running on port ${port}`));
