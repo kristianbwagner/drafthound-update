04942b610b7f829fe9b8c1758f573358616ebfdf
Kristian B. Wagner <kristianbwagner@gmail.com>
2018-07-25 21:39:56 +0200

    updated cors to cors module

--- server/index.js
+++ server/index.js
@@ -1,15 +1,13 @@
 const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
+const cors = require("cors");
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
+app.use(cors());
+
 
 // Routes
 require("./routes")(app);
