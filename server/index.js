990e1e3fc24c719b6531ddff887a6905cc2e0bf3
Kristian B. Wagner <kristianbwagner@gmail.com>
2018-07-25 20:30:33 +0200

    Added CORS headers and body parser

--- server/index.js
+++ server/index.js
@@ -1,5 +1,15 @@
 const express = require("express");
 const app = express();
+const bodyParser = require("body-parser");
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 // Routes
 require("./routes")(app);
