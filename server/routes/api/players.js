58d4213b693c19cc07e1d6a61438ad7bfc17781b
Kristian Wagner <kwa@magnetix.dk>
2018-08-06 20:43:54 +0200

    ADded players update post

--- server/routes/api/players.js
+++ server/routes/api/players.js
@@ -4,6 +4,16 @@ const Database = new Postgres({
   ssl: true
 });
 
+const pgp = require('pg-promise')({
+	capSQL: true
+});
+
+// Create connection ref to database
+const db = pgp({
+	connectionString: "postgres://gomhcbepfwkkeq:c12e3f58fd938bbbb0825806f1ac90a08cf415de754b2da8d1c4866cf2981faf@ec2-54-217-205-90.eu-west-1.compute.amazonaws.com:5432/dagkemjclktp71",
+	ssl: true
+});
+
 module.exports = (app) => {
   app.get("/api/players", (req, res) => {
 
@@ -47,4 +57,64 @@ module.exports = (app) => {
       })
     }
   })
+
+	app.post("/api/players/:playerId", (req, res) => {
+
+		// Column schema for table
+		const schema = new pgp.helpers.ColumnSet([
+			{name: "id", def: null},
+			{name: "holdet_id", def: null},
+		], {table: "players"});
+
+    const playerId = req.params.playerId;
+		const executionStart = process.hrtime();
+		const executionTime = process.hrtime(executionStart)[1] / 1000000;
+
+		var execuionStart = process.hrtime();
+		const body = req.body;
+
+		if(body.data !== undefined) {
+			const playerData = [{
+				id: playerId,
+				holdet_id: data.holdet_id
+			}]
+			insertData(playerData, schema).then(() => {
+		    const execuionTime = process.hrtime(execuionStart)[1] / 1000000;
+		    res.json({
+		      success: true,
+		      execution_time: `${execuionTime}ms`,
+		      message: "Succesfully updated"
+		    })
+			}).catch((error) => {
+				const execuionTime = process.hrtime(execuionStart)[1] / 1000000;
+				res.json({
+		      success: false,
+		      execution_time: `${execuionTime}ms`,
+		      message: "Couln't update odds data"
+		    })
+			});
+		} else {
+			const execuionTime = process.hrtime(execuionStart)[1] / 1000000;
+			res.json({
+				success: false,
+				execution_time: `${execuionTime}ms`,
+				message: "Please provide a valid data body"
+			})
+		}
+  })
+}
+
+// Create reusable function for upsert-like insert
+function insertData(data, cs) {
+	const conflictQuery = " ON CONFLICT (id) DO UPDATE SET " + cs.columns.map(x => {
+		return `${x.name} = EXCLUDED.${x.name}`;
+	}).join(', ');
+	const insert = pgp.helpers.insert(data, cs) + conflictQuery;
+	return new Promise((resolve, reject) => {
+		db.none(insert).then(() => {
+			resolve();
+		}).catch(err => {
+			reject(err);
+		});
+	});
 }
